c774b44173a14ad0e2b2969d6025de26a2a38a62
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2007-08-26 20:56:26 +0000

    meguni added and view started

--- realms/foaf/src/resources/findfriend/src/java/org/wyona/yanel/impl/resources/findfriend/FindFriendResource.java
+++ realms/foaf/src/resources/findfriend/src/java/org/wyona/yanel/impl/resources/findfriend/FindFriendResource.java
@@ -5,15 +5,53 @@
 package org.wyona.yanel.impl.resources.findfriend;
 
 import org.wyona.yanel.core.Resource;
+import org.wyona.yanel.core.api.attributes.ViewableV2;
+import org.wyona.yanel.core.attributes.viewable.View;
+import org.wyona.yanel.core.attributes.viewable.ViewDescriptor;
 
 /**
  *
  */
-public class FindFriendResource extends Resource {
+public class FindFriendResource extends Resource implements ViewableV2 {
 
     /**
      *
      */
     public FindFriendResource() {
+    }
+
+    /**
+     *
+     */
+    public boolean exists() {
+        return true;
+    }
+
+    /**
+     *
+     */
+    public String getMimeType(String viewId) {
+        return "application/xml";
+    }
+
+    /**
+     *
+     */
+    public long getSize() {
+        return -1;
+    }
+
+    /**
+     *
+     */
+    public View getView(String viewId) {
+        return null;
+    }
+
+    /**
+     *
+     */
+    public ViewDescriptor[] getViewDescriptors() {
+        return null;
     }
 }
