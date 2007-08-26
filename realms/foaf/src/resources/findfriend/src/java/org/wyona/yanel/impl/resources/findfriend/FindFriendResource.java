cc877390a161cced0673e4c7b493dec65a6ae843
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2007-08-26 20:45:38 +0000

    find a friend resource started

---
+++ realms/foaf/src/resources/findfriend/src/java/org/wyona/yanel/impl/resources/findfriend/FindFriendResource.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright 2007 Wyona
+ */
+
+package org.wyona.yanel.impl.resources.findfriend;
+
+import org.wyona.yanel.core.Resource;
+
+/**
+ *
+ */
+public class FindFriendResource extends Resource {
+
+    /**
+     *
+     */
+    public FindFriendResource() {
+    }
+}
