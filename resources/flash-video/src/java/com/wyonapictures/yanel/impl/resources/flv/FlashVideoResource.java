dbd93db55fea09de08577c304c28e36a43584480
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2007-10-28 23:09:44 +0000

    link dynamically

--- resources/flash-video/src/java/com/wyonapictures/yanel/impl/resources/flv/FlashVideoResource.java
+++ resources/flash-video/src/java/com/wyonapictures/yanel/impl/resources/flv/FlashVideoResource.java
@@ -103,76 +103,12 @@ public class FlashVideoResource extends Resource implements ViewableV2 {
 
         sb.append("<script type=\"text/javascript\">");
 
-        sb.append("var FO = {movie:\"" + PathUtil.getResourcesHtdocsPath(this) + "flvplayer.swf\",width:\"425\",height:\"350\",majorversion:\"7\",build:\"0\",bgcolor:\"#FFFFFF\",allowfullscreen:\"true\", flashvars:\"file=customers/onfilm/coop/Munster_Dinos.flv&amp;image=" + PathUtil.getResourcesHtdocsPath(this) + "wyona-pictures.jpg\" };");
+        sb.append("var FO = {movie:\"" + PathUtil.getResourcesHtdocsPath(this) + "flvplayer.swf\",width:\"425\",height:\"350\",majorversion:\"7\",build:\"0\",bgcolor:\"#FFFFFF\",allowfullscreen:\"true\", flashvars:\"file=" + getResourceConfigProperty("href") + "&amp;image=" + PathUtil.getResourcesHtdocsPath(this) + "wyona-pictures.jpg\" };");
 
         sb.append("UFO.create(FO, \"player1\");");
 
         sb.append("</script>");
         sb.append("</body>");
-
-
-/*
-        sb.append("<head>");
-        //sb.append("<script src=\"http://simile.mit.edu/timeline/api/timeline-api.js\" type=\"text/javascript\"></script>");
-        sb.append("<script src=\"" + PathUtil.getResourcesHtdocsPath(this) + "timeline-api.js\" type=\"text/javascript\"></script>");
-
-        sb.append("<script type=\"text/javascript\">");
-        sb.append("var tl;");
-
-        sb.append("var resizeTimerID = null;");
-        sb.append("function onResize() {");
-        sb.append("    if (resizeTimerID == null) {");
-        sb.append("        resizeTimerID = window.setTimeout(function() {");
-        sb.append("            resizeTimerID = null;");
-        sb.append("            tl.layout();");
-        sb.append("        }, 500);");
-        sb.append("    }");
-        sb.append("}");
-
-        sb.append("function onLoad() {");
-        sb.append("  var eventSource = new Timeline.DefaultEventSource();");
-        sb.append("  var bandInfos = [");
-        sb.append("    Timeline.createBandInfo({");
-        sb.append("        eventSource:    eventSource,");
-        sb.append("        date:           \"" + todaysDate + "\",");
-        sb.append("        width:          \"70%\",");
-        sb.append("        intervalUnit:   Timeline.DateTime.MONTH,");
-        sb.append("        intervalPixels: 100");
-        sb.append("    }),");
-        sb.append("    Timeline.createBandInfo({");
-        sb.append("showEventText:  false,");
-        sb.append("        trackHeight:    0.5,");
-        sb.append("        trackGap:       0.2,");
-
-        sb.append("        eventSource:    eventSource,");
-        sb.append("        date:           \"" + todaysDate + "\",");
-        sb.append("        width:          \"30%\",");
-        sb.append("        intervalUnit:   Timeline.DateTime.YEAR,"); 
-        sb.append("        intervalPixels: 200");
-        sb.append("    })");
-        sb.append("  ];");
-        sb.append("  bandInfos[1].syncWith = 0;");
-        sb.append("  bandInfos[1].highlight = true;");
-  
-        sb.append("  tl = Timeline.create(document.getElementById(\"my-timeline\"), bandInfos);");
-        // TODO: Check first if a query string already exists!
-        sb.append("  Timeline.loadXML(\"" + getResourceConfigProperty("href") + "?do-not-cache-timestamp=" + new java.util.Date().getTime() + "\", function(xml, url) { eventSource.loadXML(xml, url); });");
-        //sb.append("  Timeline.loadXML(\"" + getResourceConfigProperty("href") + "\", function(xml, url) { eventSource.loadXML(xml, url); });");
-        sb.append("}");
-        sb.append("</script>");
-        sb.append("<title>" + getResourceConfigProperty("title") + "</title>");
-        sb.append("</head>");
-
-        sb.append("<body onload=\"onLoad();\" onresize=\"onResize();\">");
-        sb.append("<h3>" + getResourceConfigProperty("title") + "</h3>");
-        sb.append("<p>Today's Date (server time): " + todaysDate + "</p>");
-        sb.append("<p>XML: <a href=\"" + getResourceConfigProperty("href") + "\">" + getResourceConfigProperty("href") + "</a></p>");
-
-        String height = getResourceConfigProperty("height");
-        if (height == null) height = "250px";
-        sb.append("<div id=\"my-timeline\" style=\"height: " + height + "; border: 1px solid #aaa\"></div>");
-        sb.append("</body>");
-*/
         sb.append("</html>");
         return sb;
     }
