b90559351031697fb9334a8a76cea77b210fa8c8
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2007-11-30 22:59:16 +0000

    dependencies fixed

--- realms/foaf/src/resources/findfriend/src/java/org/wyona/yanel/impl/resources/findfriend/FindFriendResource.java
+++ realms/foaf/src/resources/findfriend/src/java/org/wyona/yanel/impl/resources/findfriend/FindFriendResource.java
@@ -179,12 +179,10 @@ public class FindFriendResource extends Resource implements ViewableV2 {
      *
      */
     private Repository getProfilesRepository() throws Exception {
-        Repository repo = getRealm().getRepository();
-        String repoConfig = getResourceConfigProperty("repo-config");
-        if (repoConfig != null) {
-            repo = new RepositoryFactory().newRepository("profiles", new File(repoConfig));
-        }
-        return repo;
+        Repository repoProfiles = ((org.wyona.yanel.impl.map.FOAFRealm) getRealm()).getProfilesRepository();
+        if (repoProfiles != null) return repoProfiles;
+
+        return getRealm().getRepository();
     }
 
     /**
