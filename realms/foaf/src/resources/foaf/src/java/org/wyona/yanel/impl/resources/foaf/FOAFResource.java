c8458cfaae50a0fa1c9f2512619cd201ef15f882
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2007-12-02 22:55:55 +0000

    use string buffer for the moment because XIncludeTransformer does not propagte the namespaces

--- realms/foaf/src/resources/foaf/src/java/org/wyona/yanel/impl/resources/foaf/FOAFResource.java
+++ realms/foaf/src/resources/foaf/src/java/org/wyona/yanel/impl/resources/foaf/FOAFResource.java
@@ -147,7 +147,8 @@ public class FOAFResource extends BasicXMLResource implements IntrospectableV1, 
 */
                 return view;
             } else {
-                return getXMLView(viewId, new java.io.ByteArrayInputStream(baos.toByteArray()));
+                return getXMLView(viewId, new StringBufferInputStream(sb.toString()));
+                //return getXMLView(viewId, new java.io.ByteArrayInputStream(baos.toByteArray()));
             }
         } catch (java.io.FileNotFoundException e) {
             log.error(e);
