e917d224ef11ebf675859a444a11ff45541cf0f3
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2007-12-03 11:21:22 +0000

    first method implemented

---
+++ resources/soap/src/java/ch/naz/yanel/Constants.java
@@ -0,0 +1,24 @@
+package ch.naz.yanel;
+
+public class Constants {
+	public static final String SOAP_MIME_TYPE = "application/soap+xml";
+	
+	public static final String NS_URI = "http//www.naz.ch/eld/wyona/1.0";
+	
+	// Look into RoobricService.wsdl for details
+	public static final String TITLE_EN_TAG = "title_en";
+	public static final String TITLE_DE_TAG = "title_de";
+	public static final String TITLE_IT_TAG = "title_it";
+	public static final String TITLE_FR_TAG = "title_fr";
+	
+	public static final String NAME_TAG = "name";
+	public static final String PARENT_NODE_PATH_TAG = "parent-node-path";
+	public static final String AXIS_TAG = "axis";
+	
+	public static final String CREATE_RUBRIC_RESPONSE_TAG = "CreateRubricResponse";
+	public static final String CREATE_RUBRIC_TAG = "CreateRubric";
+	
+	public static final String EXCEPTION_MESSAGE_TAG = "exception-message";
+	public static final String NODE_PATH_TAG = "node-path";
+	
+}
--- resources/soap/src/java/org/wyona/yanel/impl/resources/soap/SOAPResource.java
+++ resources/soap/src/java/org/wyona/yanel/impl/resources/soap/SOAPResource.java
@@ -1,58 +1,102 @@
-/*
- * Copyright 2007 Wyona
- */
-
 package org.wyona.yanel.impl.resources.soap;
 
 import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
+import javax.xml.namespace.NamespaceContext;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 import org.wyona.yanel.core.Resource;
 import org.wyona.yanel.core.api.attributes.ViewableV2;
 import org.wyona.yanel.core.attributes.viewable.View;
 import org.wyona.yanel.core.attributes.viewable.ViewDescriptor;
 
-public class SOAPResource extends Resource implements ViewableV2{
-	private static String ENVELOPE =
-		"<?xml version='1.0'?>"+
-		"<soap:Envelope xmlns:soap='http://www.w3.org/2001/12/soap-envelope' soap:encodingStyle='http://www.w3.org/2001/12/soap-encoding'>"+
-			"<soap:Body xmlns:o='http://www.optaros.com'>" +
-				"<o:exampleResponse>" +
-					" Hello World!" +
-				"</o:exampleResponse>" +
-			"</soap:Body>" +
-		"</soap:Envelope>";
+import ch.naz.yanel.Constants;
+import ch.naz.yanel.CreateRubricBean;
+
+/**
+ *
+ */
+public class SOAPResource extends Resource implements ViewableV2 {
+	private static final NamespaceContext NAMESPACE_CONTEXT;
+	static{
+		final Map namespaces = new HashMap();
+		namespaces.put("soapenv", "http://schemas.xmlsoap.org/soap/envelope/");
+		namespaces.put("naz", "http//www.naz.ch/eld/wyona/1.0");
+		
+		NAMESPACE_CONTEXT = new NamespaceContext(){
+			public String getNamespaceURI(String prefix) {
+				return (String)namespaces.get(prefix);
+			}
+			
+			public String getPrefix(String namespaceURI) {
+				Set s = namespaces.entrySet();
+				for (Iterator i = s.iterator(); i.hasNext();) {
+					Map.Entry e = (Map.Entry)i.next();
+					if(namespaceURI.equals(e.getValue())){
+						return (String)e.getKey();
+					}
+				}
+				return null;
+			}
+			
+			public Iterator getPrefixes(String namespaceURI) {
+				return new HashSet(namespaces.keySet()).iterator();
+			}
+			
+		};
+	}
 	
-    public SOAPResource() {
-    }
-    
-    public boolean exists() throws Exception {
-    	return true;
-    }
-    
+	
+	private static final String SOAP_MIME_TYPE = "application/soap+xml";
+	
+	public boolean exists() throws Exception {
+		return true;
+	}
+	
+	public long getSize() throws Exception {
+		return -1;
+	}
+
     /**
      *
      */
-    private String getMimeType(String viewId) throws Exception {
-        if (getResourceConfigProperty("mime-type") != null) return getResourceConfigProperty("mime-type");
-    	return "application/soap+xml";
-    }
-    
-    public long getSize() throws Exception {
-    	return ENVELOPE.getBytes().length;
-    }
-    
     public View getView(String viewId) throws Exception {
-    	
-    	System.out.println(super.getConfiguration().getProperty("encoding"));
-    	
-    	View v = new View();
-    	v.setInputStream( new ByteArrayInputStream(ENVELOPE.getBytes()));
-    	// This is important!
-    	v.setMimeType(getMimeType(viewId));
-    	return v;
+        View v = new View();
+        v.setMimeType(SOAP_MIME_TYPE);
+		
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        dbf.setNamespaceAware(true);
+        Document soapMessage = dbf.newDocumentBuilder().parse(getEnvironment().getRequest().getInputStream());
+		
+        XPath xpath = XPathFactory.newInstance().newXPath();
+        xpath.setNamespaceContext(NAMESPACE_CONTEXT);
+        Element payload = (Element)xpath.evaluate("/soapenv:Envelope/soapenv:Body/*", soapMessage, XPathConstants.NODE);
+		
+        ByteArrayInputStream bais = new ByteArrayInputStream(new ServiceInterceptor().handleRequest(payload).getBytes());
+        v.setInputStream(bais);
+		
+        return v;
     }
-    
+
+    /**
+     *
+     */
     public ViewDescriptor[] getViewDescriptors() {
-    	return null;
+        // TODO Auto-generated method stub
+        return null;
     }
 }
