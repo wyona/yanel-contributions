9987bed1b2badbd04f3b5844cea92b92e4955cfe
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2007-12-11 23:48:40 +0000

    get name started

--- realms/foaf/src/resources/shared/src/java/org/wyona/foaf/impl/basics/PersonImpl.java
+++ realms/foaf/src/resources/shared/src/java/org/wyona/foaf/impl/basics/PersonImpl.java
@@ -10,6 +10,7 @@ import java.io.InputStream;
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
 import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.rdf.model.impl.PropertyImpl;
 
 import org.apache.log4j.Category;
 
@@ -20,6 +21,8 @@ public class PersonImpl implements Person {
 
     private static Category log = Category.getInstance(PersonImpl.class);
 
+    private static String foafNamespace = "http://xmlns.com/foaf/0.1/";
+
     /**
      *
      */
@@ -28,6 +31,10 @@ public class PersonImpl implements Person {
         model.read(in, "");
         Resource person = model.getResource("foaf:Person");
         log.error("DEBUG: Resource: " + person);
+        //String name = person.getProperty(new PropertyImpl(foafNamespace, "name")).toString();
+        //log.error("DEBUG: Name: " + name);
+        //String givenName = person.getProperty(new PropertyImpl(foafNamespace, "givenname")).toString();
+        //String familyName = person.getProperty(new PropertyImpl(foafNamespace, "family_name")).toString();
     }
 
     /**
