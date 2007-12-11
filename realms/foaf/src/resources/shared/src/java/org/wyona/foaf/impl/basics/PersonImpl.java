b436a6effae53bf700ed2982ee8a0e23338b3637
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2007-12-11 23:17:19 +0000

    jena used for implementation

--- realms/foaf/src/resources/shared/src/java/org/wyona/foaf/impl/basics/PersonImpl.java
+++ realms/foaf/src/resources/shared/src/java/org/wyona/foaf/impl/basics/PersonImpl.java
@@ -5,10 +5,30 @@ package org.wyona.foaf.impl.basics;
 
 import org.wyona.foaf.api.basics.Person;
 
+import java.io.InputStream;
+
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.Resource;
+
+import org.apache.log4j.Category;
+
 /**
  *
  */
 public class PersonImpl implements Person {
+
+    private static Category log = Category.getInstance(PersonImpl.class);
+
+    /**
+     *
+     */
+    public PersonImpl(InputStream in) {
+        Model model = ModelFactory.createDefaultModel();
+        model.read(in, "");
+        Resource person = model.getResource("foaf:Person");
+        log.error("DEBUG: Resource: " + person);
+    }
 
     /**
      *
