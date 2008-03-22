5d824a96eb9c3b56da21620655463f855647ce07
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2008-03-22 23:52:21 +0000

    create nodes

--- realms/foaf/src/resources/import-foaf/src/java/org/wyona/foaf/yanel/impl/resources/impfoaf/ImportFOAFResource.java
+++ realms/foaf/src/resources/import-foaf/src/java/org/wyona/foaf/yanel/impl/resources/impfoaf/ImportFOAFResource.java
@@ -7,7 +7,10 @@ package org.wyona.foaf.yanel.impl.resources.impfoaf;
 import org.wyona.yanel.impl.resources.usecase.ExecutableUsecaseResource;
 import org.wyona.yanel.impl.resources.usecase.UsecaseException;
 
+import org.wyona.yarep.core.Node;
+
 import java.io.ByteArrayInputStream;
+import java.io.File;
 import java.io.InputStream;
 
 import org.apache.log4j.Logger;
@@ -25,7 +28,7 @@ public class ImportFOAFResource extends ExecutableUsecaseResource {
      * This method is executed when submitting the form provided in the default view (probably implemented as a jelly template).
      */
     public void execute() throws UsecaseException {
-
+        doImportFOAFFiles();
     }
 
     /*
@@ -39,7 +42,7 @@ public class ImportFOAFResource extends ExecutableUsecaseResource {
      * Implement some test which are tested before the usecase will e executed
      */
     public boolean checkPreconditions() throws UsecaseException {
-        return false;
+        return true;
     }
 
     /**
@@ -49,7 +52,7 @@ public class ImportFOAFResource extends ExecutableUsecaseResource {
         try {
             String importDir = getResourceConfigProperty("import-directory");
             if (importDir != null) {
-                if (new java.io.File(importDir).isDirectory()) {
+                if (new File(importDir).isDirectory()) {
                     return importDir;
                 } else {
                     return "No such directory: " + importDir;
@@ -60,6 +63,25 @@ public class ImportFOAFResource extends ExecutableUsecaseResource {
         } catch(Exception e) {
             log.error(e, e);
             return "Exception: " + e.getMessage();
+        }
+    }
+
+    /**
+     *
+     */
+    private void doImportFOAFFiles() {
+        File importDir = new File(getImportDirectory());
+        File[] filesAndDirs = importDir.listFiles();
+        for (int i = 0; i < filesAndDirs.length; i++) {
+            if (filesAndDirs[i].isFile()) {
+                log.error("DEBUG: File: " + filesAndDirs[i].getAbsolutePath());
+                try {
+                    Node newFOAFNode = ((org.wyona.yanel.impl.map.FOAFRealm) getRealm()).getProfilesRepository().getNode("/profiles").addNode(filesAndDirs[i].getName(), org.wyona.yarep.core.NodeType.RESOURCE);
+                    newFOAFNode.setMimeType("application/xml");
+                } catch(Exception e) {
+                    log.error(e, e);
+                }
+            }
         }
     }
 }
