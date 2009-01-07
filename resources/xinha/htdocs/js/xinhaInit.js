5ed4d095ea01f325a3a49658ad31ffdfa9e294de
simon <simon@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2009-01-07 23:53:47 +0000

    obsolete

--- resources/xinha/htdocs/js/xinhaInit.js
+++
@@ -1,9 +0,0 @@
-        tinyMCE.init({
-            mode : "textareas",
-            theme : "advanced",
-            theme_advanced_layout_manager: "RowLayout",
-            theme_advanced_containers: "container1,mceEditor",
-            theme_advanced_container_container1: "bold,italic,underline,separator,bullist,separator,undo,redo,separator,link,unlink,separator,formatselect,image,code",
-            plugins : "fullpage",
-            entity_encoding : "raw"
-        });
