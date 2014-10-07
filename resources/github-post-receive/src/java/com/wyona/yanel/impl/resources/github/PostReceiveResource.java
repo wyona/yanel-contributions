0c20e0c14386ecef583de8e9bd1050b284195c9d
Michael Wechner <michael.wechner@wyona.com>
2014-10-07 16:14:33 +0200

    bean introduced

--- resources/github-post-receive/src/java/com/wyona/yanel/impl/resources/github/PostReceiveResource.java
+++ resources/github-post-receive/src/java/com/wyona/yanel/impl/resources/github/PostReceiveResource.java
@@ -46,7 +46,8 @@ public class PostReceiveResource extends Resource implements ViewableV2  {
         String json = getJSon();
         if (json != null) {
             // INFO: Parse json, e.g. http://json.parser.online.fr/
-            String[] modifiedFiles = getModifiedFiles(json);
+            CommitInfoBean bean = getCommitInformation(json);
+            addContinuousIntegrationTask(bean);
         } else {
             log.error("No json received!");
         }
@@ -61,28 +62,53 @@ public class PostReceiveResource extends Resource implements ViewableV2  {
     /**
      *
      */
-    private String[] getModifiedFiles(String json) {
+    private void addContinuousIntegrationTask(CommitInfoBean bean) {
+        log.warn("DEBUG: Add continuous integration task ...");
+
+        String[] files= bean.getModifiedFiles();
+        for (int i = 0; i < files.length; i++) {
+            log.warn("DEBUG: Modified file: " + files[i]);
+        }
+
+        log.warn("DEBUG: Branch: " + bean.getBranch());
+
+        log.warn("DEBUG: Repository: " + bean.getRepositoryName() + ", " + bean.getRepositoryURL());
+    }
+
+    /**
+     *
+     */
+    private CommitInfoBean getCommitInformation(String json) {
+        CommitInfoBean bean = new CommitInfoBean();
+
         // TODO: https://jsonp.java.net/
         org.json.JSONObject jsonObj = new org.json.JSONObject(json);
 
         String branch = jsonObj.getString("ref");
         log.warn("DEBUG: Branch: " + branch);
+        bean.setBranch(branch);
 
         String repoName = jsonObj.getJSONObject("repository").getString("name");
         log.warn("DEBUG: Repository name: " + repoName);
+        bean.setBranch(repoName);
 
         String repoURL = jsonObj.getJSONObject("repository").getString("url");
         log.warn("DEBUG: Repository URL: " + repoURL);
+        bean.setRepositoryURL(repoURL);
 
+        java.util.ArrayList<String> files = new java.util.ArrayList<String>();
         org.json.JSONArray commits = jsonObj.getJSONArray("commits");
         for (int i = 0; i < commits.length(); i++) {
             org.json.JSONArray modifiedFiles = commits.getJSONObject(i).getJSONArray("modified");
             for (int k = 0; k < modifiedFiles.length(); k++) {
-                log.warn("DEBUG: Modified file: " + modifiedFiles.get(k));
+                String file = modifiedFiles.get(k).toString();
+                files.add(file);
+                log.warn("DEBUG: Modified file: " + file);
             }
         }
+        bean.setModifiedFiles(files.toArray(new String[0]));
 
-        return null;
+        return bean;
     }
 
     /**
@@ -120,5 +146,72 @@ public class PostReceiveResource extends Resource implements ViewableV2  {
      */
     public ViewDescriptor[] getViewDescriptors() {
         return null;
+    }
+}
+
+/**
+ *
+ */
+class CommitInfoBean {
+
+    private String[] modifiedFiles;
+    private String branch;
+    private String repoName;
+    private String repoURL;
+
+    /**
+     *
+     */
+    public void setBranch(String branch) {
+        this.branch = branch;
+    }
+
+    /**
+     *
+     */
+    public String getBranch() {
+        return branch;
+    }
+
+    /**
+     *
+     */
+    public void setRepositoryName(String name) {
+        this.repoName = name;
+    }
+
+    /**
+     *
+     */
+    public String getRepositoryName() {
+        return repoName;
+    }
+
+    /**
+     *
+     */
+    public void setRepositoryURL(String url) {
+        this.repoURL = url;
+    }
+
+    /**
+     *
+     */
+    public String getRepositoryURL() {
+        return repoURL;
+    }
+
+    /**
+     *
+     */
+    public void setModifiedFiles(String[] files) {
+        this.modifiedFiles = files;
+    }
+
+    /**
+     *
+     */
+    public String[] getModifiedFiles() {
+        return modifiedFiles;
     }
 }
